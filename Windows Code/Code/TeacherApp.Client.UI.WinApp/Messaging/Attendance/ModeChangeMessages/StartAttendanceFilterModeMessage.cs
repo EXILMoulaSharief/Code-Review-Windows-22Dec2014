c2133b6065097510ecf63c2c3f1c10c01dda8d77
Moula Sharief <moula.sharief@exilant.com>
2014-12-22 22:10:53 +0530

    Code Deleted

--- Windows Code/Code/TeacherApp.Client.UI.WinApp/Messaging/Attendance/ModeChangeMessages/StartAttendanceFilterModeMessage.cs
+++
@@ -1,8 +0,0 @@
-using TeacherApp.Client.Shared.UI.iOS.Messenger;
-
-namespace TeacherApp.Client.UI.iOS.Messaging.Attendance.ModeChangeMessages
-{
-    internal class StartAttendanceFilterModeMessage : Message
-    {
-    }
-}
